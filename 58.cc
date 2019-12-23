c9ddcc48430100157b74efb304a633356ad8beb2
shigzz <shigz97@gmail.com>
2019-12-23 11:09:43 +0800

    formated

--- 58.cc
+++ 58.cc
@@ -1,20 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int lengthOfLastWord(string s) {
-    if (s[s.length()-1]==' '){
-        return lengthOfLastWord(s.substr(0,s.length()-1));
+int lengthOfLastWord(string s)
+{
+    if (s[s.length() - 1] == ' ')
+    {
+        return lengthOfLastWord(s.substr(0, s.length() - 1));
     }
-    int i,j=0;
-    for (i=0;i<s.length();i++){
-        if (s[i]==' '){
-            j=i+1;
+    int i, j = 0;
+    for (i = 0; i < s.length(); i++)
+    {
+        if (s[i] == ' ')
+        {
+            j = i + 1;
         }
     }
-    return i-j;
+    return i - j;
 }
 
-int main(){
-    cout<<lengthOfLastWord("a ");
+int main()
+{
+    cout << lengthOfLastWord("a ");
 }
--- 67.cc
+++ 67.cc
@@ -1,28 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-string addBinary(string a, string b) {
-    reverse(a.begin(),a.end());
-    reverse(b.begin(),b.end());
+string addBinary(string a, string b)
+{
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
     vector<char> res;
     int flag = 0;
-    for (int i=0,j=0;i<a.length()||j<b.length();i++,j++){
-        int ta = i<a.length()?a[i]-48:0;
-        int tb = j<b.length()?b[i]-48:0;
-        int tmp = ta+tb+flag;
-        flag = tmp/2;
-        tmp = tmp%2;
-        res.push_back(char(tmp+48));
+    for (int i = 0, j = 0; i < a.length() || j < b.length(); i++, j++)
+    {
+        int ta = i < a.length() ? a[i] - 48 : 0;
+        int tb = j < b.length() ? b[i] - 48 : 0;
+        int tmp = ta + tb + flag;
+        flag = tmp / 2;
+        tmp = tmp % 2;
+        res.push_back(char(tmp + 48));
     }
-    if (flag){
+    if (flag)
+    {
         res.push_back('1');
     }
-    reverse(res.begin(),res.end());
-    return string(res.begin(),res.end());
+    reverse(res.begin(), res.end());
+    return string(res.begin(), res.end());
 }
 
-int main(){
-    string a = addBinary("111","111");
-    cout<<a<<endl;
+int main()
+{
+    string a = addBinary("111", "111");
+    cout << a << endl;
     return 0;
 }
