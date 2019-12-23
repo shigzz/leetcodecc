104bec7d6eeeefb93b790515a79dab3dd5f610d7
shigzz <shigz97@gmail.com>
2019-12-23 12:55:47 +0800

    formated

--- 59.cc
+++ 59.cc
@@ -1,37 +1,47 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> generateMatrix(int n) {
+vector<vector<int>> generateMatrix(int n)
+{
     vector<vector<int>> res;
-    for (int i=0;i<n;i++){
+    for (int i = 0; i < n; i++)
+    {
         res.push_back(vector<int>(n));
     }
-    int l,r,b,f;
-    l=0,r=n-1,f=0,b=n-1;
-    int total=n*n;
-    int num=1;
-    while(num<=total){
+    int l, r, b, f;
+    l = 0, r = n - 1, f = 0, b = n - 1;
+    int total = n * n;
+    int num = 1;
+    while (num <= total)
+    {
         //cout<<num<<endl;
-        for (int i=l;i<=r;i++) res[f][i]=num++;
+        for (int i = l; i <= r; i++)
+            res[f][i] = num++;
         f++;
-        for (int i=f;i<=b;i++) res[i][r]=num++;
+        for (int i = f; i <= b; i++)
+            res[i][r] = num++;
         r--;
-        for (int i=r;i>=l;i--) res[b][i]=num++;
+        for (int i = r; i >= l; i--)
+            res[b][i] = num++;
         b--;
-        for (int i=b;i>=f;i--) res[i][l]=num++;
+        for (int i = b; i >= f; i--)
+            res[i][l] = num++;
         l++;
     }
     return res;
 }
 
-int main(){
+int main()
+{
     vector<vector<int>> mat = generateMatrix(3);
-    for (vector<vector<int>>::iterator it=mat.begin();it!=mat.end();it++){
+    for (vector<vector<int>>::iterator it = mat.begin(); it != mat.end(); it++)
+    {
         vector<int> row = *it;
-        for (vector<int>::iterator itt=row.begin();itt!=row.end();itt++){
-            cout<<*itt<<" ";
+        for (vector<int>::iterator itt = row.begin(); itt != row.end(); itt++)
+        {
+            cout << *itt << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
